734d6b16a9cfc7e328079e493673f3a79f03ff72
Zinadin Zidan <zidan.roking@gmail.com>
2021-07-23 21:14:07 +0600

    mojito: rootdir: Import rootdir files
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>
    
    mojito: rootdir: Remove unused init files
    
    Co-authored-by: Demon000 <demonsingur@gmail.com>
    Co-authored-by: Arian <arian.kulmer@web.de>
    Change-Id: I351d0164aff886bfc5233e9daa881f271a43045f
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>
    
    mojito: rootdir: Import init.recovery.qcom rc
    
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I78c8e91f573ca8512e96cd7e76f46e220fccfe07
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>
    
    mojito: rootdir: Configure zram from separate fstab
    
    Change-Id: I3d093f06b08ad1cc049325d0136b60cab70c29d0
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>
    
    mojito: rootdir: Tune zram performance
    
    -Switch to LZ4 for better overall performance
    -Set max_comp_streams to num of cores since upstream also moves this to
    percpu.
    -Set page-cluster to 0 as the incremental cost of reading 1 page at a
    time is negligible for zram
    
    Bug: 38249616
    Test: boot and run zram-perf showing better performance
    Change-Id: I0b92b246d773db85aa03d033b2cecee54347cbd1
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>
    
    mojito: rootdir: Increase ZRAM to 2GB
    
    Test: boots, works
    bug 119988524
    
    Change-Id: I09272e7c753569d16f2be0771835bfdbdc0bf270
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>
    
    mojito: rootdir: Make zram swap /data/per_boot
    
    Put zram writeback backing store into /data/per_boot
    
    Bug: 140882488
    Test: cat /sys/block/loop9/loop/backing_file shows /data/per_boot/zram_swap
    Change-Id: I096b8a82e6a9fdd10ea4d5ae3cff3c4ed5332acf
    Signed-off-by: Minchan Kim <minchan@google.com>
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>
    
    mojito: rootdir: Correct zram parameter names
    
    [libfs_mgr]Warning: unknown flag: zram_loopback_path=/data/per_boot/zram_swap
    [libfs_mgr]Warning: unknown flag: zram_loopback_size=512M
    
    We don't use zram_loopback_path any longer so remove it.
    We also changed zram_loopback_size with zram_backingdev_size so correct it.
    
    Bug: 145330273
    Bug: 145116439
    Test: confirmed there is no warning and zram backing device was configured
    
    Change-Id: I51be34c59203813a2f493eee6ab442c9940b75d7
    Signed-off-by: Minchan Kim <minchan@google.com>
    Signed-off-by: YH_Lin <yhli@google.com>
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>
    
    mojito: rootdir: Chown /dev/xiaomi-touch
    
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: Id7a8cc40418d24327209f6f0180e62ae68dd60b9
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>
    
    mojito: rootdir: Import device fstab from stock
    
    Co-authored-by: Albert I <kras@raphielgang.org>
    Change-Id: Ibc910f2349f09bb7af2dbb1d87b8acaeb749b0dd
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -6,3 +6,15 @@
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/mojito/mojito-vendor.mk)
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    init.qcom.post_boot.sh \
+    init.qcom.sh \
+    fstab.zram \
+    fstab.qcom \
+    init.qcom.rc \
+    init.qcom.usb.rc \
+    init.recovery.qcom.rc \
+    init.target.rc \
+    ueventd.qcom.rc
