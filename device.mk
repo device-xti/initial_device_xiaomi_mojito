27d2da3d8a343f1f2728461398d4c3d728517617
Zinadin Zidan <zidan.roking@gmail.com>
2021-07-23 19:19:15 +0600

    mojito: Build drm clearkey packages
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I9666d5ba4679bbaf173fd3450450cc5403edfebb
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -117,6 +117,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.4.vendor \
+    android.hardware.drm-service.clearkey
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1.vendor \
