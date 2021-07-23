6984f81283b6775d18e772c82f26e8e2c6924a34
Zinadin Zidan <zidan.roking@gmail.com>
2021-07-23 14:37:25 +0600

    mojito: Add arch and bootloader flags
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I462d0552e24730d181be15eca2d1affa2c06a5f0
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,25 @@
 
 DEVICE_PATH := device/xiaomi/mojito
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a76
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a76
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := sm6150
+TARGET_NO_BOOTLOADER := true
+
 # Security patch level
 VENDOR_SECURITY_PATCH := 2023-10-01
 
