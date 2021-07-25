27ed01d7180894168652af09b562afe3937b88a0
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-25 21:00:05 +0600

    mojito: Import sepolicy from Xiaomi SM8250
    * https://github.com/xiaomi-sm8250-devs/android_device_xiaomi_sm8250-common/commits/lineage-18.1/sepolicy
    
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I98272bba31a0232bb8f2b6956722b68bd1f63819
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>
    
    mojito: sepolicy: Allow gpu services to read graphics props
    
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I77565e6396e6ccc2fc3d283a4edb00e81c8b1960
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>
    
    mojito: sepolicy: Allow system_app to access zram sysfs nodes
    
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I78b72681addeab990e89f3c9dee253f40c1c83b9
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>
    
    mojito: sepolicy: Drop M4DEFS
    
    Change-Id: I43c99687e099d029d99a807bb5384151601c9d36
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,13 @@ DEVICE_PATH := device/xiaomi/mojito
 # Security patch level
 VENDOR_SECURITY_PATCH := 2023-10-01
 
+# Sepolicy
+TARGET_SEPOLICY_DIR := msmsteppe
+include device/qcom/sepolicy_vndr/SEPolicy.mk
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
+BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
+
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
