21c79afcfb650ee72087bb22e469f340670f6754
Arian <arian.kulmer@web.de>
2021-12-25 01:00:45 +0600

    mojito: sepolicy: Move xiaomi specific property labels to vendor
    Signed-off-by: Zinadin Zidan <zidan.roking@gmail.com>
    Change-Id: I4565fe8f24ade48dc8e72c3865dbc717638ba4b2
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>
    
    mojito: Import AIDL Fingerprint HAL from hardware/xiaomi
    
    - This is imported from [1].
    
    [1]: https://github.com/pixelos-aosp/hardware_xiaomi/tree/old/aidl/fingerprint
    
    Change-Id: I1e0594b88d5230f8c99a6efb0a7dafcbf6ea137a
    Signed-off-by: OliverSyx <173170056+OliverSyx@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -9,7 +9,12 @@ $(call inherit-product, vendor/xiaomi/mojito/mojito-vendor.mk)
 
 # Fingerprint
 PRODUCT_PACKAGES += \
+<<<<<<< HEAD
     android.hardware.biometrics.fingerprint@2.1-service.mojito
+=======
+    android.hardware.biometrics.fingerprint@2.1.vendor \
+    android.hardware.biometrics.fingerprint-service.sunny
+>>>>>>> 5284c55 (sunny: Import AIDL Fingerprint HAL from hardware/xiaomi)
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
