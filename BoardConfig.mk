488e0f62bd9da6ce36fbcd0a8c3fe113b22dbaa5
OliverSyx <173170056+OliverSyx@users.noreply.github.com>
2025-04-28 20:53:15 +0700

    mojito: Initial device tree

---
+++ BoardConfig.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/xiaomi/mojito
+
+# Security patch level
+VENDOR_SECURITY_PATCH := 2023-10-01
+
+# Inherit the proprietary files
+include vendor/xiaomi/mojito/BoardConfigVendor.mk
